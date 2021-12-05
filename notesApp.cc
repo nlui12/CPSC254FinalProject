4f57048ae9bf0b8c6b21877f6a444300e02f0e83
Nick <nicklui1998@gmail.com>
2021-12-05 14:07:34 -0800

    restructured code to be more Object-oriented

--- notesApp.cc
+++ notesApp.cc
@@ -1,12 +1,11 @@
-#include <gtkmm.h>
-#include <gtkmm/application.h>
-int main(int argc, char *argv[]){
-auto app = Gtk::Application::create(argc, argv);
-Gtk::Window myWindow;
-Gtk::Button myButton("Hello World");
-myWindow.set_default_size(600, 360);
-myWindow.set_position(Gtk::WIN_POS_CENTER);
-myWindow.add(myButton);
-myButton.show();
-return app->run(myWindow);
+#include "notesApp.h"
+#include <gtkmm/stock.h>
+#include <iostream>
+
+notesApp::notesApp(){
+  add(m_Box);
+}
+
+notesApp::~notesApp()
+{
 }
