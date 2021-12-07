93a617601e78cc53e08e0ddf799f00de57d6f24b
omar-mann <52722867+omar-mann@users.noreply.github.com>
2021-12-07 03:01:19 -0800

    save functionality added

--- notesApp.cc
+++ notesApp.cc
@@ -1,7 +1,8 @@
 #include "notesApp.h"
 #include <gtkmm/stock.h>
 #include <iostream>
-
+#include <fstream>
+#include <string>
 notesApp::notesApp()
 : m_Box(Gtk::ORIENTATION_VERTICAL),
   d_Box(Gtk::ORIENTATION_VERTICAL),
@@ -10,95 +11,18 @@ notesApp::notesApp()
   h3_Box(Gtk::ORIENTATION_HORIZONTAL)
 
 {
+
   set_title("Notes");
-  set_default_size(800,400);
-
-
-  add(m_Box);
-
-  //d_Box.add(h2_Box);
-  //h2_Box.set_vexpand(false);
-
-  //File|New sub menu:
-  actionGroup = Gtk::ActionGroup::create();
-  actionGroup->add(Gtk::Action::create("FileNewStandard",
-              Gtk::Stock::NEW, "_New", "Create a new file"),
-          sigc::mem_fun(*this, &notesApp::on_menu_file_new_generic));
-
-  actionGroup->add(Gtk::Action::create("FileNewFile",
-              Gtk::Stock::NEW, "New File", "Create a new foo"),
-          sigc::mem_fun(*this, &notesApp::on_menu_file_new_generic));
-
-  actionGroup->add(Gtk::Action::create("FileNewWindow",
-              Gtk::Stock::NEW, "_New Window", "Create a new goo"),
-          sigc::mem_fun(*this, &notesApp::on_menu_file_new_generic));
-
-  actionGroup->add(Gtk::Action::create("FileMenu", "File"));
-  actionGroup->add(Gtk::Action::create("FileNew", Gtk::Stock::NEW));
-  actionGroup->add(Gtk::Action::create("FileQuit", Gtk::Stock::QUIT),
-          sigc::mem_fun(*this, &notesApp::on_menu_file_quit));
-
-  myUIManager = Gtk::UIManager::create();
-
-  myUIManager->insert_action_group(actionGroup);
-  add_accel_group(myUIManager->get_accel_group());
-
-Glib::ustring ui_info =
-        "<ui>"
-        "  <menubar name='MenuBar'>"
-        "    <menu action='FileMenu'>"
-        "      <menu action='FileNew'>"
-        "        <menuitem action='FileNewStandard'/>"
-        "        <menuitem action='FileNewFile'/>"
-        "        <menuitem action='FileNewWindow'/>"
-        "      </menu>"
-        "      <separator/>"
-        "      <menuitem action='FileQuit'/>"
-        "    </menu>"
-        "    <menu action='EditMenu'>"
-        "      <menuitem action='EditCopy'/>"
-        "      <menuitem action='EditPaste'/>"
-        "      <menuitem action='EditSomething'/>"
-        "    </menu>"
-        "    <menu action='ChoicesMenu'>"
-        "      <menuitem action='ChoiceOne'/>"
-        "      <menuitem action='ChoiceTwo'/>"
-        "    </menu>"
-        "    <menu action='HelpMenu'>"
-        "      <menuitem action='HelpAbout'/>"
-        "    </menu>"
-        "  </menubar>"
-        "  <toolbar  name='ToolBar'>"
-        "    <toolitem action='FileNewStandard'/>"
-        "    <toolitem action='FileQuit'/>"
-        "  </toolbar>"
-        "</ui>";
-
-  try
-  {
-    myUIManager->add_ui_from_string(ui_info);
-  }
-  catch(const Glib::Error& ex)
-  {
-    std::cerr << "building menus failed: " <<  ex.what();
-  }
-  //add menu widget menu container
-  Gtk::Widget* pMenubar = myUIManager->get_widget("/MenuBar");
-  if(pMenubar)
-    m_Box.pack_start(*pMenubar, Gtk::PACK_SHRINK);
-
-  //add toolbar widget to menu container
-  Gtk::Widget* pToolbar = myUIManager->get_widget("/ToolBar") ;
-  if(pToolbar)
-    m_Box.pack_start(*pToolbar, Gtk::PACK_SHRINK);
+  set_default_size(800,400); //window size resolution
+  add(m_Box); //adding a main box widget
 
   //add box which contain all window elements below the menu
   m_Box.add(d_Box);
-  //add h2_box to main box
+  //add h2_box to main box widget
   d_Box.add(h2_Box);
   h2_Box.set_vexpand(false);
 
-    //set and add entry to box
+  //creating an add entry text region
   notesEntry->set_text("add notes");
   set_focus_child(*notesEntry);
   notesEntry->set_text(notesEntry->get_text() + " to list");
@@ -106,57 +30,56 @@ Glib::ustring ui_info =
   notesEntry->signal_changed().connect(sigc::mem_fun(*this, &notesApp::on_text_changed));
   h2_Box.pack_start(*notesEntry, true, true, 5);
 
-  //add new task button
+  //adds new note
   b_add->signal_clicked().connect(sigc::mem_fun(*this, &notesApp::on_addEntry_clicked));
   h2_Box.pack_start(*b_add, false, true, 5);
 
-
-
-  Glib::ustring someTask = "some task";
-  //add Button which saves editing
-  b_saveEdit->signal_clicked().connect(sigc::mem_fun(*this, &notesApp::onSaveEdit_clicked));
-  b_saveEdit->set_sensitive(false);
-  h2_Box.pack_start(*b_saveEdit, false, true, 5);
-
-  //delete button
-  b_delete->signal_clicked().connect(sigc::mem_fun(*this, &notesApp::on_delete_clicked));
-  b_delete->set_sensitive(false);
-  h2_Box.pack_start(*b_delete, false, true, 5);
+  Glib::ustring someNote = "some note";
 
   m_ScrolledWindow.add(m_TreeView);
-
   m_ScrolledWindow.set_policy(Gtk::POLICY_AUTOMATIC,Gtk::POLICY_AUTOMATIC);
 
-    //Create the Tree model:
+  //Create the Tree model to display list of notes:
   m_refTreeModel = Gtk::ListStore::create(m_Columns);
-
   m_TreeView.set_model(m_refTreeModel);
 
-row = *(m_refTreeModel->append());
-row[m_Columns.m_col_id]=1;
-row[m_Columns.m_col_percentage]=15;
-row[m_Columns.m_col_name] = "Test text";
+count = 1;
+
+//reads from notes.txt and appends lines to our TreeModel
+//serves as the save functionality of the program
+std::string note_content;
+std::string line;
+std::ifstream inFile ("notes.txt");
+if (inFile.is_open())
+{
+  while(std::getline(inFile,line))
+  {
+    note_content = line;
+    note_content = line.substr(13);
+
+    row = *(m_refTreeModel->append());
+    row[m_Columns.m_col_id]=count;
+    row[m_Columns.m_col_name] = note_content;
+    count++;
+  }
+  inFile.close();
+}
+
+//row = *(m_refTreeModel->append());
+//row[m_Columns.m_col_id]=1;
+//row[m_Columns.m_col_name] = "Test text";
 
 m_refTreeSelection = m_TreeView.get_selection();
 m_refTreeSelection->signal_changed().connect( sigc::mem_fun(*this,&notesApp::on_selection_changed));
 
-count=1;
-
+//Header bar at the top of the list
 m_TreeView.append_column("ID", m_Columns.m_col_id);
-m_TreeView.append_column("Task completed", m_Columns.m_col_percentage);
-m_TreeView.append_column("Task Description", m_Columns.m_col_name);
+m_TreeView.append_column("Note Entry", m_Columns.m_col_name);
 
 d_Box.add(h3_Box);
 h3_Box.set_vexpand(true);
 
 h3_Box.pack_start(m_ScrolledWindow,true,true,5);
-
-//fill in the treeview Model
-
-
-
-
-
 
   show_all_children();
 }
@@ -165,99 +88,41 @@ notesApp::~notesApp()
 {
 }
 
-void notesApp::on_menu_file_new_generic()
-{
-   std::cout << "A File|New menu item was selected." << std::endl;
-}
-
 void notesApp::on_text_changed()
 {
-    bool is_button_active = b_saveEdit->get_sensitive();
-
-    if(any_row_selected && !is_button_active) {
-        b_saveEdit->set_sensitive(true);
-    }
-
      std::cout << "text has changed" << std::endl;
 }
 
 void notesApp::on_addEntry_clicked() {
-    count++;
+    //count++;
 
     row = *(m_refTreeModel->append());
     row[m_Columns.m_col_id] = count;
     row[m_Columns.m_col_name] = notesEntry->get_text();
-    row[m_Columns.m_col_percentage] = m_HScale.get_value();
+    std::ofstream outObj;
+    outObj.open("notes.txt", std::ios_base::app);
+    outObj << "note ID: " << count << " - " << notesEntry->get_text() << std::endl;
+    outObj.close();
+    count++;
 }
 
-void notesApp::add_entry(Glib::ustring task, int percentage)
+void notesApp::add_entry(Glib::ustring note)
 {
-  std::cout << task << std::endl;
-    std::cout << percentage << std::endl;
+  std::cout << note << std::endl;
 
 }
 
 void notesApp::on_selection_changed()
 {
     any_row_selected = true;
-    b_saveEdit->set_sensitive(true);
-    b_delete->set_sensitive(true);
 
     Gtk::TreeModel::iterator iter = m_refTreeSelection->get_selected();
     Gtk::TreeModel::Row row = *iter;
 
-    Glib::ustring task_string = row.get_value(m_Columns.m_col_name);
-    int selected_percentage = row.get_value(m_Columns.m_col_percentage);
+    Glib::ustring note_string = row.get_value(m_Columns.m_col_name);
     unsigned int selected_id = row.get_value(m_Columns.m_col_id);
 
-    notesEntry->set_text(task_string);
-    double scale_value = (double) selected_percentage;
-    m_HScale.set_value(scale_value);
+    notesEntry->set_text(note_string);
 
   std::cout<< selected_id<< std::endl;
-}
-
-void notesApp::onSaveEdit_clicked()
-{
-    b_saveEdit->set_sensitive(false);
-
-    Gtk::TreeModel::iterator iter = m_refTreeSelection->get_selected();
-    Gtk::TreeModel::Row row = *iter;
-
-    Glib::ustring new_entry = notesEntry->get_text();
-    row.set_value(m_Columns.m_col_name, new_entry);
-    int new_percentage = m_HScale.get_value();
-    row.set_value(m_Columns.m_col_percentage, new_percentage);
-}
-
-
-void notesApp::on_menu_file_quit()
-{
-             //Gtk::MESSAGE_ERROR
-         Gtk::MessageDialog quitDialog("Are you sure you want to Quit?",
-                                        false,
-                                        Gtk::MESSAGE_ERROR,
-                                        Gtk::BUTTONS_YES_NO,
-                                        true);
-            quitDialog.set_title("Quit Dialog");
-            //save dialog result in order to handle the click
-            int result = quitDialog.run();
-        switch(result) {
-            // default YES response id
-            case(yes_response):
-                hide();
-
-                break;
-            // default NO response id
-            case(no_response):
-                // hide previews dialog, or it gets messy
-                quitDialog.hide();
-                break;
-            }
-}
-
-void notesApp::on_delete_clicked()
-{
-    Gtk::TreeModel::iterator store_iter = m_refTreeSelection->get_selected();
-    m_refTreeModel->erase(store_iter);
 }
--- notesApp.h
+++ notesApp.h
@@ -2,51 +2,30 @@
 #define NOTESAPP_H
 
 #include <gtkmm.h>
-
 class notesApp : public Gtk::Window
 {
     public:
       notesApp();
       virtual ~notesApp();
-      static const int yes_response = -8;
-      static const int no_response = -9;
-      static const int ok_response = -5;
-      Gtk::Label *topText = Gtk::manage(new Gtk::Label());
-      Gtk::Label *topTextRight = Gtk::manage(new Gtk::Label());
-      Gtk::Grid *grid = Gtk::manage(new Gtk::Grid);
       Gtk::Entry *notesEntry = Gtk::manage(new Gtk::Entry());
-      Gtk::Button *b_add = Gtk::manage(new Gtk::Button("Add New Task"));
-      Gtk::Button *b_saveEdit = Gtk::manage(new Gtk::Button("Save Edit"));
-      Gtk::Button *b_delete = Gtk::manage(new Gtk::Button("Delete Task"));
-      Gtk::TreeViewColumn* pColumn = Gtk::manage(new Gtk::TreeViewColumn);
+      Gtk::Button *b_add = Gtk::manage(new Gtk::Button("Add New Note"));
       Glib::RefPtr<Gtk::AccelGroup> group = Gtk::AccelGroup::create();
       Glib::RefPtr<Gtk::Adjustment> m_adjustment;
 
     protected:
-    void on_menu_file_new_generic();
-    void on_menu_file_quit();
-    void on_menu_others();
-
-    void on_menu_choices_one();
-    void on_menu_choices_two();
     void on_addEntry_clicked();
-    void add_entry(Glib::ustring task, int percentage);
-    void on_help_clicked();
-    void onSaveEdit_clicked();
+    void add_entry(Glib::ustring note);
     void on_selection_changed();
-    void on_delete_clicked();
     void on_text_changed();
-    void on_scale_changed();
 
     class ModelColumns : public Gtk::TreeModel::ColumnRecord
   {
     public:
 
     ModelColumns()
-    { add(m_col_id); add(m_col_percentage); add(m_col_name);}
+    { add(m_col_id); add(m_col_name);}
 
     Gtk::TreeModelColumn<unsigned int> m_col_id;
-    Gtk::TreeModelColumn<int> m_col_percentage;
     Gtk::TreeModelColumn<Glib::ustring> m_col_name;
   };
 
@@ -61,11 +40,6 @@ class notesApp : public Gtk::Window
   Gtk::Box h1_Box;
   Gtk::Box h2_Box;
   Gtk::Box h3_Box;
-  Gtk::EventBox event_box;
-  Gtk::Label m_Label;
-  Gtk::Scale m_HScale;
-  Gdk::Pixbuf *pixmap;
-
   Gtk::ScrolledWindow m_ScrolledWindow;
   Gtk::TreeView m_TreeView;
   Glib::RefPtr<Gtk::ListStore> m_refTreeModel;
